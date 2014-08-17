ca052c3ba98e7e4cc0cc136df57e8610ef0d63ff
Scott Kaplan <scott@kaplans.com>
2014-08-17 17:50:09 +0000

    create dst files with standard names and create config.xml entries

--- tools/artwork.rb
+++ tools/artwork.rb
@@ -5,147 +5,226 @@ require 'RMagick'
 include Magick
 
 ARTWORKS = [
+            #
+            # iOS
+            #
             { src_file:          'icon_512x512.png',
-              dst_file:          'res/ios/icon-60.png',
-              target_resolution: '60x60',
-            },
-            { src_file:          'icon_512x512.png',
-              dst_file:          'res/ios/icon-60@2x.png',
-              target_resolution: '120x120',
-            },
-            { src_file:          'icon_512x512.png',
-              dst_file:          'res/ios/icon-76.png',
-              target_resolution: '76x76',
-            },
-            { src_file:          'icon_512x512.png',
-              dst_file:          'res/ios/icon-76@2x.png',
-              target_resolution: '152x152',
-            },
-            { src_file:          'icon_512x512.png',
-              dst_file:          'res/ios/icon-40.png',
-              target_resolution: '40x40',
-            },
-            { src_file:          'icon_512x512.png',
-              dst_file:          'res/ios/icon-40@2x.png',
-              target_resolution: '80x80',
-            },
-            { src_file:          'icon_512x512.png',
-              dst_file:          'res/ios/icon-57.png',
               target_resolution: '57x57',
+              image_type:        'icon',
+              platform:          'ios',
+              comment:           'iPhone & iPod Touch non retina iOS6',
             },
             { src_file:          'icon_512x512.png',
-              dst_file:          'res/ios/icon-57@2x.png',
               target_resolution: '114x114',
+              image_type:        'icon',
+              platform:          'ios',
+              comment:           'iPhone & iPod Touch retina iOS6',
             },
             { src_file:          'icon_512x512.png',
-              dst_file:          'res/ios/icon-72.png',
               target_resolution: '72x72',
+              image_type:        'icon',
+              platform:          'ios',
+              comment:           'iPad non retina iOS6',
             },
             { src_file:          'icon_512x512.png',
-              dst_file:          'res/ios/icon-72@2x.png',
               target_resolution: '144x144',
+              image_type:        'icon',
+              platform:          'ios',
+              comment:           'iPad retina iOS6',
             },
             { src_file:          'icon_512x512.png',
-              dst_file:          'res/ios/icon-small.png',
-              target_resolution: '29x29',
+              target_resolution: '76x76',
+              image_type:        'icon',
+              platform:          'ios',
+              comment:           'iPad non retina iOS7',
             },
             { src_file:          'icon_512x512.png',
-              dst_file:          'res/ios/icon-small@2x.png',
-              target_resolution: '58x58',
+              target_resolution: '152x152',
+              image_type:        'icon',
+              platform:          'ios',
+              comment:           'iPad retina iOS7',
             },
             { src_file:          'icon_512x512.png',
-              dst_file:          'res/ios/icon-50.png',
-              target_resolution: '50x50',
+              target_resolution: '60x60',
+              image_type:        'icon',
+              platform:          'ios',
+              comment:           'iPhone & iPod Touch non retina iOS7',
             },
             { src_file:          'icon_512x512.png',
-              dst_file:          'res/ios/icon-50@2x.png',
-              target_resolution: '100x100',
+              target_resolution: '120x120',
+              image_type:        'icon',
+              platform:          'ios',
+              comment:           'iPhone & iPod Touch retina iOS7',
             },
 
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/ios/Default.png',
               target_resolution: '320x480',
+              image_type:        'splash',
+              platform:          'ios',
+              comment:           'iPhone & iPod Touch non retina',
             },
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/ios/Default@2x.png',
               target_resolution: '640x960',
+              image_type:        'splash',
+              platform:          'ios',
+              comment:           'iPhone & iPod Touch retina 4-',
             },
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/ios/Default-568h@2x.png',
               target_resolution: '640x1136',
+              image_type:        'splash',
+              platform:          'ios',
+              comment:           'iPhone & iPod Touch retina 5+',
             },
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/ios/Default-Portrait.png',
               target_resolution: '768x1024',
+              image_type:        'splash',
+              platform:          'ios',
+              comment:           'iPad non retina',
             },
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/ios/Default-Landscape.png',
               target_resolution: '1024x768',
+              image_type:        'splash',
+              platform:          'ios',
+              comment:           'iPad non retina',
             },
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/ios/Default-Portrait@2x.png',
               target_resolution: '1536x2048',
+              image_type:        'splash',
+              platform:          'ios',
+              comment:           'iPad retina',
             },
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/ios/Default-Landscape@2x.png',
               target_resolution: '2048x1536',
+              image_type:        'splash',
+              platform:          'ios',
+              comment:           'iPad retina',
             },
             
+            #
+            # Android
+            #
             { src_file:          'icon_512x512.png',
-              dst_file:          'res/android/ldpi.png',
               target_resolution: '36x36',
+              image_type:        'icon',
+              platform:          'android',
+              extra_parms:       'gap:density="ldpi"',
             },
             { src_file:          'icon_512x512.png',
-              dst_file:          'res/android/mdpi.png',
               target_resolution: '48x48',
+              image_type:        'icon',
+              platform:          'android',
+              extra_parms:       'gap:density="mdpi"',
             },
             { src_file:          'icon_512x512.png',
-              dst_file:          'res/android/hdpi.png',
               target_resolution: '72x72',
+              image_type:        'icon',
+              platform:          'android',
+              extra_parms:       'gap:density="hdpi"',
             },
             { src_file:          'icon_512x512.png',
-              dst_file:          'res/android/xhdpi.png',
               target_resolution: '96x96',
+              image_type:        'icon',
+              platform:          'android',
+              extra_parms:       'gap:density="xhdpi"',
             },
+            { src_file:          'icon_512x512.png',
+              target_resolution: '144x144',
+              image_type:        'icon',
+              platform:          'android',
+              extra_parms:       'gap:density="xxhdpi"',
+            },
+            { src_file:          'icon_512x512.png',
+              target_resolution: '192x192',
+              image_type:        'icon',
+              platform:          'android',
+              extra_parms:       'gap:density="xxhdpi"',
+            },
+
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/android/screen-ldpi-portrait.png',
               target_resolution: '200x320',
+              image_type:        'splash',
+              platform:          'android',
             },
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/android/screen-mdpi-portrait.png',
               target_resolution: '320x480',
+              image_type:        'splash',
+              platform:          'android',
             },
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/android/screen-hdpi-portrait.png',
               target_resolution: '480x800',
+              image_type:        'splash',
+              platform:          'android',
             },
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/android/screen-xhdpi-portrait.png',
               target_resolution: '720x1280',
+              image_type:        'splash',
+              platform:          'android',
             },
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/android/screen-ldpi-landscape.png',
               target_resolution: '320x200',
+              image_type:        'splash',
+              platform:          'android',
             },
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/android/screen-mdpi-landscape.png',
               target_resolution: '480x320',
+              image_type:        'splash',
+              platform:          'android',
             },
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/android/screen-hdpi-landscape.png',
               target_resolution: '800x480',
+              image_type:        'splash',
+              platform:          'android',
             },
             { src_file:          'splash_screen_720x1280.png',
-              dst_file:          'res/android/screen-xhdpi-landscape.png',
               target_resolution: '1280x720',
-            }
+              image_type:        'splash',
+              platform:          'android',
+            },
+            #
+            # Blackberry
+            #
+            { src_file:          'icon_512x512.png',
+              target_resolution: '80x80',
+              image_type:        'icon',
+              platform:          'blackberry',
+            },
+            { src_file:          'icon_512x512.png',
+              target_resolution: '80x80',
+              image_type:        'icon',
+              platform:          'blackberry',
+              extra_parms:       'gap:state="hover"',
+            },
+            { src_file:          'splash_screen_720x1280.png',
+              target_resolution: '768x1024',
+              image_type:        'splash',
+              platform:          'blackberry',
+            },
+            #
+            # Winphone
+            #
+            { src_file:          'icon_512x512.png',
+              target_resolution: '57x57',
+              image_type:        'icon',
+              platform:          'winphone',
+            },
+            { src_file:          'icon_512x512.png',
+              target_resolution: '72x72',
+              image_type:        'icon',
+              platform:          'winphone',
+              extra_parms:       'gap:role="background"',
+            },
+            { src_file:          'splash_screen_720x1280.png',
+              target_resolution: '480x800',
+              image_type:        'splash',
+              platform:          'winphone',
+            },
            ]
 
-IMAGE_BASE_DIR = '/home/geopeers/sinatra/geopeers/public/images'
+IMAGE_BASE_DIR = '/home/geopeers/phonegap/geopeers/images'
 
-def create_image (image_info)
-  image_file = IMAGE_BASE_DIR + '/' + image_info[:src_file]
-  src_img = Magick::Image::read(image_file)[0]
+def process_image (image_info)
+  src_image_file = IMAGE_BASE_DIR + '/' + image_info[:src_file]
+  src_img = Magick::Image::read(src_image_file)[0]
 
   # scale src_img down so it fits within target_resolution
   
@@ -166,12 +245,38 @@ def create_image (image_info)
     # place the scaled src image on top of the white background
     dst_img.composite!(src_img_scaled,CenterGravity,OverCompositeOp)
   end
-  dst_file = IMAGE_BASE_DIR + '/' + image_info[:dst_file]
-  dst_img.write(dst_file)
+  dst_file = 'res/' + image_info[:platform] + '/' + image_info[:image_type] + '_' + image_info[:target_resolution] + File.extname(src_image_file)
+  dst_img.write(IMAGE_BASE_DIR + '/' + dst_file)
+
+  if (image_info[:image_type] == 'icon')
+    config_ent = '<icon'
+  elsif (image_info[:image_type] == 'splash')
+    config_ent = '<gap:splash'
+  else
+    return
+  end
+  config_ent += " src=\"#{dst_file}\""
+  config_ent += " gap:platform=\"#{image_info[:platform]}\""
+  config_ent += " width=\"#{dst_width}\""
+  config_ent += " height=\"#{dst_height}\""
+  if (image_info[:extra_parms])
+    config_ent += ' '
+    config_ent += image_info[:extra_parms]
+  end
+  config_ent += " />"
+  if (image_info[:comment])
+    config_ent += ' <!-- '
+    config_ent += image_info[:comment]
+    config_ent += ' -->'
+  end
+  config_ent
 end
 
-def create_artworks (artworks)
-  artworks.each { |image_info| create_image(image_info) }
+def process_artworks (artworks)
+  artworks.each do |image_info|
+    config_ent = process_image(image_info)
+    puts config_ent
+  end
 end
 
-create_artworks(ARTWORKS)
+process_artworks(ARTWORKS)
