adbdea1aabe7dbd6693337510db8525b8a30679b
Scott Kaplan <scott@kaplans.com>
2014-11-02 22:40:53 +0000

    Phonegap

--- tools/build.rb
+++ tools/build.rb
@@ -5,38 +5,16 @@ require '/home/geopeers/sinatra/geopeers/geo.rb'
 require 'git'
 require 'fileutils'
 
-def git_push
-  github = Github.new basic_auth: 'scott@kaplans.com:scottkaplan1'
-
-  contents = github::Client::Repos::Contents.new
-  index_file = '/home/geopeers/phonegap/geopeers/index.html'
-  index_html = File.read(index_file)
-  file = contents.find path: index_file
-  contents.update 'scott@kaplans.com', 'geopeers', index_file,
-  path: index_file,
-  message: 'Automated commit',
-  content: index_html,
-  sha: file.sha
-end
-
-def git_pull
-  project_dir = '/home/geopeers/phonegap/geopeers/webapp'
-  remote_rep = 'https://magtogo.gitsrc.com/git/geopeers.git'
-  local_rep = '/home/geopeers/phonegap/geopeers/webapp'
-  name = 'geopeers'
-  g = Git.clone(remote_rep, name, {path: local_rep} )
-  # git = Git.init(project_dir, {repository: local_rep})
-  # git.pull
-end
+Phonegap_dir = "/home/geopeers/phonegap/geopeers"
 
 def write_index_html
   html = create_index({is_phonegap: true})
-  output_file = '/home/geopeers/phonegap/geopeers/index.html'
+  output_file = "#{Phonegap_dir}/index.html"
   File.open(output_file, 'w') { |file| file.write(html) }
 end
 
 def edit_config_xml
-  config_xml_file = "/home/geopeers/phonegap/geopeers/config.xml"
+  config_xml_file = "#{Phonegap_dir}/config.xml"
   config_xml = File.read(config_xml_file)
   build_id = get_build_id()
   puts build_id
@@ -46,7 +24,7 @@ end
 
 def pull_webapp
   remote_rep = 'https://scott:scott_kaplan@magtogo.gitsrc.com/git/geopeers.git'
-  local_rep = '/home/geopeers/phonegap/geopeers/webapp'
+  local_rep = "#{Phonegap_dir}/webapp"
   cmd = "cd #{local_rep}; rm -rf *; git clone #{remote_rep}"
   puts cmd
   result = `#{cmd}`
@@ -56,19 +34,23 @@ def copy_local_assets
   for asset_type in ['css', 'images', 'js']
     # clear out the local assets in the phonegap repo
     # Don't just rm -rf the directory since we want to save the repo info
-    phonegap_repo = "/home/geopeers/phonegap/geopeers/#{asset_type}"
+    phonegap_repo = "#{Phonegap_dir}/#{asset_type}"
     phonegap_files = Dir.glob("#{phonegap_repo}/*")
     FileUtils.rm_r (phonegap_files)
 
-    webapp_repo = "/home/geopeers/phonegap/geopeers/webapp/geopeers/public/#{asset_type}"
+    webapp_repo = "#{Phonegap_dir}/webapp/geopeers/public/#{asset_type}"
     webapp_files = Dir.glob("#{webapp_repo}/*")
     FileUtils.cp_r(webapp_files, phonegap_repo)
   end
 end
 
 def update_phonegap_repo
-  # git commit
-  # git push
+  cmd = "cd #{Phonegap_dir}; git commit -a -m 'Phonegap'"
+  puts cmd
+  `cmd`
+  cmd = "cd #{Phonegap_dir}; git push"
+  puts cmd
+  `cmd`
 end
 
 def build
