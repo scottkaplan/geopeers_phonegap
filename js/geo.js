1df4e6b450476c8a8020851d1b683e6c4e6b0537
scott <scott@kaplans.com>
2014-12-02 23:51:35 +0000

    Phonegap

--- js/geo.js
+++ js/geo.js
@@ -41,6 +41,13 @@ function is_phonegap () {
     return (window.location.href.match (/^file:/));
 }
 
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+	results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 function have_native_app () {
     // is the native app installed on this device?
     if (registration.reg_info &&
@@ -264,13 +271,6 @@ function geo_ajax_fail_callback (data, textStatus, jqXHR) {
     $('#registration_form_spinner').hide();
     $('#share_location_form_spinner').hide();
     return;
-}
-
-function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-	results = regex.exec(location.search);
-    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
 function gps_position_succeeded (post_func, position) {
@@ -943,7 +943,6 @@ var map_mgr = {
     update_canvas_pos: function() {
 	var content_height = $('#geo_info').height() + 65;
 	$('#content').css('top', content_height+'px');
-	console.log ("content_height="+content_height);
 	return;
     },
     create: function(position) {
@@ -975,7 +974,6 @@ var map_mgr = {
 	    map_mgr.update_canvas_pos();
 	    
 	    var map = $('#map_canvas').gmap('get','map');
-	    console.log(map.getZoom());
 	    // And again in the bounds_changed callback if the bounds have changed
 	    google.maps.event.trigger(map, 'resize');
 	}
